2e2a47f8701c3290cdeec65c25d572d3a56316fd
Otto F. Quintanilha <ottofq@gmail.com>
2020-03-12 22:22:38 -0300

    add pagina de avisos

---
+++ src/pages/Avisos/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  padding: 5px;
+  flex: 1;
+  margin-bottom: 5px;
+`;
+
+export const InfoCard = styled.View`
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 5px;
+  margin: 5px;
+  background-color: #e2e2e2;
+`;
+
+export const Title = styled.Text`
+  padding: 5px;
+  font-family: 'PTSans-Bold';
+  border-bottom-width: 1px;
+`;
+
+export const InfoContent = styled.Text`
+  font-family: 'PTSans-Regular';
+  padding: 5px;
+  line-height: 25px;
+  font-size: 14px;
+  color: #111111;
+`;
