80f842c3710a7dbd049ef135274808ddde7f771f
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-20 20:47:58 -0300

    refactor checkbox  styles

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { TouchableNativeFeedback } from 'react-native';
 import { Checkbox } from 'react-native-paper';
 
-import { Container, Label } from './styles';
+import { colors } from '../../styles';
+import * as S from './styles';
 
 const CheckboxItem = ({ onPress, label, status }) => {
   return (
     <TouchableNativeFeedback onPress={onPress}>
-      <Container>
-        <Checkbox.Android status={status} onPress={onPress} color="#004B82" />
-        <Label>{label}</Label>
-      </Container>
+      <S.Container>
+        <Checkbox.Android
+          status={status}
+          onPress={onPress}
+          color={colors.primary}
+        />
+        <S.Label>{label}</S.Label>
+      </S.Container>
     </TouchableNativeFeedback>
   );
 };
