ffef6e85f020b1f9de2deb2eefed0d05ffe05c2c
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-20 20:49:19 -0300

    fix spaces in progressBar

--- src/components/ProgressBar/styles.js
+++ src/components/ProgressBar/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
-import {ProgressBar} from 'react-native-paper';
-import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { ProgressBar } from 'react-native-paper';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export const Progress = styled(ProgressBar)`
   height: ${hp(2)}px;
