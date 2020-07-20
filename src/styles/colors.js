e5fd3d1e9b4fa99d20c3d21ca48ca37af7f9fa66
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-20 20:46:18 -0300

    add styles variables

---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+export default {
+  primary: '#004B82',
+  secondary: '#426696',
+  tertiary: '#f6fafd',
+
+  grayDark: '#969696',
+};
---
+++ src/styles/fonts.js
@@ -0,0 +1,12 @@
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+export default {
+  superSmall: hp(1.6),
+  smaller: hp(1.8),
+  small: hp(2.3),
+  regular: hp(2.5),
+  big: hp(2.8),
+  bigger: hp(3),
+  superBig: hp(3.2),
+  giant: hp(3.5),
+};
