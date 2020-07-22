a8e6a72ded8a5e22f04bf37733068fd93ca0f247
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-21 23:23:37 -0300

    change form route names

--- src/pages/Forms/Main/index.js
+++ src/pages/Forms/Main/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Intro from '../Intro';
 import Done from '../Done';
@@ -22,34 +21,26 @@ export default function Main() {
       screenOptions={{
         headerShown: false,
         headerLeft: null,
-        //   headerTitleAlign: 'center',
-        //   title: 'QUESTIONÁRIO',
-        //   headerTitleStyle: {
-        //     fontSize: hp(3),
-        //     color: '#fff',
-        //     fontFamily: 'PTSans-Bold',
-        //   },
-        //   headerStyle: {backgroundColor: '#004B82', height: hp(7)},
-        //
       }}
-      initialRouteName="Intro">
+      initialRouteName="Intro"
+    >
       <Stack.Screen
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
         name="Intro"
         component={Intro}
       />
 
-      <Stack.Screen name="Questionario passo 1" component={Step1} />
-      <Stack.Screen name="Questionário passo 2" component={Step2} />
-      <Stack.Screen name="Questionário passo 3" component={Step3} />
-      <Stack.Screen name="Questionário passo 4" component={Step4} />
-      <Stack.Screen name="Questionário passo 5" component={Step5} />
-      <Stack.Screen name="Questionário passo 6" component={Step6} />
-      <Stack.Screen name="Questionário passo 7" component={Step7} />
-      <Stack.Screen name="Questionário passo 8" component={Step8} />
-      <Stack.Screen name="Questionário passo 9" component={Step9} />
+      <Stack.Screen name="step-1" component={Step1} />
+      <Stack.Screen name="step-2" component={Step2} />
+      <Stack.Screen name="step-3" component={Step3} />
+      <Stack.Screen name="step-4" component={Step4} />
+      <Stack.Screen name="step-5" component={Step5} />
+      <Stack.Screen name="step-6" component={Step6} />
+      <Stack.Screen name="step-7" component={Step7} />
+      <Stack.Screen name="step-8" component={Step8} />
+      <Stack.Screen name="step-9" component={Step9} />
       <Stack.Screen
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
         name="Done"
         component={Done}
       />
