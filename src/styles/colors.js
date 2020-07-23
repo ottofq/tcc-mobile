4cb32b4aa76ca33b6b0be8c2d274d24c8167d06e
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-22 22:38:59 -0300

    add grayscale color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,7 +3,10 @@ export default {
   secondary: '#426696',
   tertiary: '#f6fafd',
 
-  grayDark: '#969696',
+  grayLight: '#d2d2d2',
+  grayMedium: '#969696',
+  grayDark: '#4a4a4a',
 
+  black: '#111111',
   error: '#B00020',
 };
