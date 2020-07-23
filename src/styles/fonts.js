782080df0678f774c80f41e7b5b898df8a0936a6
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-22 22:39:46 -0300

    change smaller size

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -2,7 +2,7 @@ import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default {
   superSmall: hp(1.6),
-  smaller: hp(1.8),
+  smaller: hp(2),
   small: hp(2.3),
   regular: hp(2.5),
   big: hp(2.8),
