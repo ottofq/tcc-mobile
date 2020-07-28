2d2269187ce1a559190470a3d108dab139e249a8
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-27 21:12:54 -0300

    refactor styles of news details screen

--- src/pages/News/Details/index.js
+++ src/pages/News/Details/index.js
@@ -1,44 +1,38 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import HTMLView from 'react-native-htmlview';
-import { StyleSheet, View } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useRoute, useNavigation } from '@react-navigation/native';
 
 import * as S from './styles';
 
 const Details = () => {
   const { params } = useRoute();
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.goBack();
+  };
 
   return (
-    <View>
-      <HTMLView stylesheet={styles} value={`<div>${params.data}</div>`} />
-    </View>
+    <S.Container>
+      <S.Header onPress={handlePress}>
+        <S.ArrowContainer>
+          <Icon name="arrow-left" color="#969696" size={32} />
+        </S.ArrowContainer>
+        <S.TitleContainer>
+          <S.Title>{params.data.titulo}</S.Title>
+        </S.TitleContainer>
+      </S.Header>
+
+      <S.Content>
+        <HTMLView
+          stylesheet={S.styles}
+          value={`<div>${params.data.descricao}</div>`}
+        />
+      </S.Content>
+    </S.Container>
   );
 };
-
-const styles = StyleSheet.create({
-  p: {
-    marginTop: 5,
-    marginBottom: 5,
-    lineHeight: 20,
-  },
-  strong: {
-    fontWeight: 'bold',
-  },
-  ul: {
-    marginTop: 5,
-    marginBottom: 5,
-    borderWidth: 1,
-  },
-  li: {
-    marginTop: 5,
-    marginBottom: 5,
-    lineHeight: 20,
-  },
-  ol: {
-    marginTop: 5,
-    marginBottom: 5,
-  },
-});
 
 export default Details;
