4b382c5602c5dec14f7bdbd93a4ce74d9a45ef14
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-03 18:40:16 -0300

    refactor step 6 form

--- src/pages/Forms/Step6/index.js
+++ src/pages/Forms/Step6/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { Button } from 'react-native-paper';
 import { KeyboardAvoidingView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -10,28 +10,9 @@ import ProgressBar from '../../../components/ProgressBar';
 import * as S from './styles';
 
 const Step6 = () => {
-  const { register, handleSubmit, setValue } = useForm();
-  const [doencaCardiovascular, setDoencaCardiovascular] = useState(false);
-  const [hipertensaoArterial, setHipertensaoArterial] = useState(false);
-  const [obesidade, setObesidade] = useState(false);
-  const [dislipidemias, setDislipidemias] = useState(false);
-  const [diabetes, setDiabetes] = useState(false);
-  const [doencaArterialCoronariana, setDoencaArterialCoronariana] = useState(
-    false
-  );
-  const [doenca, setDoenca] = useState(false);
+  const { handleSubmit, setValue, control, watch } = useForm();
   const navigation = useNavigation();
   const { params } = useRoute();
-
-  useEffect(() => {
-    setValue('patologias_familia.fam_doenca_cardiovascular', true);
-    setValue('patologias_familia.fam_hipertensao_arterial', false);
-    setValue('patologias_familia.fam_obesidade', false);
-    setValue('patologias_familia.fam_dislipidemias', false);
-    setValue('patologias_familia.fam_diabetes', false);
-    setValue('patologias_familia.fam_doenca_arterial_coronariana', false);
-    setValue('patologias_familia.patologias_familia_outras', false);
-  }, [setValue]);
 
   function handleButtonNext(data) {
     const obj = { ...params, ...data };
@@ -39,23 +20,15 @@ const Step6 = () => {
       ...obj,
     });
   }
-  function handlerCheckbox(field, state, setState) {
-    if (!doenca) {
-      setState(!state);
-      setValue(field, !state);
-    }
-  }
 
-  function handlerCheckboxDoenca() {
-    setDoenca(!doenca);
-    setValue('patologias_familia.fam_doenca_cardiovascular', true);
-    setValue('patologias_familia.fam_hipertensao', false);
-    setValue('patologias_familia.fam_obesidade', false);
-    setValue('patologias_familia.fam_dislipidemias', false);
-    setValue('patologias_familia.fam_diabetes', false);
-    setValue('patologias_familia.fam_doenca_arterial_coronariana', false);
-    setValue('patologias_familia.patologias_familia_outras', false);
-  }
+  const handleCheckboxStatus = (value) => {
+    if (watch('patologias_familia.nao_tenho_patologias'))
+      return 'indeterminate';
+
+    if (value) return 'checked';
+
+    return 'unchecked';
+  };
 
   function handleButtonPrev() {
     navigation.goBack();
@@ -70,154 +43,162 @@ const Step6 = () => {
             Há histórico de presença, de alguma das patologias abaixo, na sua
             família? Se sim, quais?
           </S.TitleCheckboxGroup>
-          <CheckBoxItem
-            label="Doença cardiovascular"
-            status={
-              doenca
-                ? 'indeterminate'
-                : 'unchecked' && doencaCardiovascular
-                ? 'checked'
-                : 'unchecked'
-            }
-            ref={register('patologias_familia.fam_doenca_cardiovascular')}
-            onPress={() =>
-              handlerCheckbox(
-                'patologias_familia.fam_doenca_cardiovascular',
-                doencaCardiovascular,
-                setDoencaCardiovascular
-              )
-            }
+
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Doença cardiovascular"
+                status={handleCheckboxStatus(value)}
+                onPress={() =>
+                  setValue(
+                    'patologias_familia.fam_doenca_cardiovascular',
+                    !value
+                  )
+                }
+              />
+            )}
+            name="patologias_familia.fam_doenca_cardiovascular"
+            control={control}
+            defaultValue={false}
           />
 
-          <CheckBoxItem
-            label="Hipertensão arterial"
-            status={
-              doenca
-                ? 'indeterminate'
-                : 'unchecked' && hipertensaoArterial
-                ? 'checked'
-                : 'unchecked'
-            }
-            ref={register('patologias_familia.fam_hipertensao')}
-            onPress={() =>
-              handlerCheckbox(
-                'patologias_familia.fam_hipertensao',
-                hipertensaoArterial,
-                setHipertensaoArterial
-              )
-            }
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Hipertensão arterial"
+                status={handleCheckboxStatus(value)}
+                onPress={() =>
+                  setValue('patologias_familia.fam_hipertensao', !value)
+                }
+              />
+            )}
+            name="patologias_familia.fam_hipertensao"
+            control={control}
+            defaultValue={false}
           />
 
-          <CheckBoxItem
-            label="Obesidade"
-            status={
-              doenca
-                ? 'indeterminate'
-                : 'unchecked' && obesidade
-                ? 'checked'
-                : 'unchecked'
-            }
-            ref={register('patologias_familia.fam_obesidade')}
-            onPress={() =>
-              handlerCheckbox(
-                'patologias_familia.fam_obesidade',
-                obesidade,
-                setObesidade
-              )
-            }
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Obesidade"
+                status={handleCheckboxStatus(value)}
+                onPress={() =>
+                  setValue('patologias_familia.fam_obesidade', !value)
+                }
+              />
+            )}
+            name="patologias_familia.fam_obesidade"
+            control={control}
+            defaultValue={false}
           />
 
-          <CheckBoxItem
-            label="Dislipidemias"
-            status={
-              doenca
-                ? 'indeterminate'
-                : 'unchecked' && dislipidemias
-                ? 'checked'
-                : 'unchecked'
-            }
-            ref={register('patologias_familia.fam_dislipidemias')}
-            onPress={() =>
-              handlerCheckbox(
-                'patologias_familia.fam_dislipidemias',
-                dislipidemias,
-                setDislipidemias
-              )
-            }
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Dislipidemias"
+                status={handleCheckboxStatus(value)}
+                onPress={() =>
+                  setValue('patologias_familia.fam_dislipidemias', !value)
+                }
+              />
+            )}
+            name="patologias_familia.fam_dislipidemias"
+            control={control}
+            defaultValue={false}
           />
 
-          <CheckBoxItem
-            label="Diabetes"
-            status={
-              doenca
-                ? 'indeterminate'
-                : 'unchecked' && diabetes
-                ? 'checked'
-                : 'unchecked'
-            }
-            ref={register('patologias_familia.fam_diabetes')}
-            onPress={() =>
-              handlerCheckbox(
-                'patologias_familia.fam_diabetes',
-                diabetes,
-                setDiabetes
-              )
-            }
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Diabetes"
+                status={handleCheckboxStatus(value)}
+                onPress={() =>
+                  setValue('patologias_familia.fam_diabetes', !value)
+                }
+              />
+            )}
+            name="patologias_familia.fam_diabetes"
+            control={control}
+            defaultValue={false}
           />
 
-          <CheckBoxItem
-            label="Doença Arterial Coronariana"
-            status={
-              doenca
-                ? 'indeterminate'
-                : 'unchecked' && doencaArterialCoronariana
-                ? 'checked'
-                : 'unchecked'
-            }
-            ref={register('patologias_familia.fam_doenca_arterial_coronariana')}
-            onPress={() =>
-              handlerCheckbox(
-                'patologias_familia.fam_doenca_arterial_coronariana',
-                doencaArterialCoronariana,
-                setDoencaArterialCoronariana
-              )
-            }
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Doença Arterial Coronariana"
+                status={handleCheckboxStatus(value)}
+                onPress={() =>
+                  setValue(
+                    'patologias_familia.fam_doenca_arterial_coronariana',
+                    !value
+                  )
+                }
+              />
+            )}
+            name="patologias_familia.fam_doenca_arterial_coronariana"
+            control={control}
+            defaultValue={false}
           />
 
-          <CheckBoxItem
-            label="Não possuo doenças"
-            status={
-              doencaCardiovascular ||
-              hipertensaoArterial ||
-              obesidade ||
-              dislipidemias ||
-              doencaArterialCoronariana ||
-              diabetes
-                ? 'indeterminate'
-                : 'unchecked' && doenca
-                ? 'checked'
-                : 'unchecked'
-            }
-            onPress={() => handlerCheckboxDoenca()}
+          <Controller
+            render={({ value }) => (
+              <CheckBoxItem
+                label="Não possuo patologias na família"
+                status={value ? 'checked' : 'unchecked'}
+                onPress={() => {
+                  setValue('patologias_familia.nao_tenho_patologias', !value);
+                  setValue(
+                    'patologias_familia.fam_doenca_cardiovascular',
+                    false
+                  );
+                  setValue('patologias_familia.fam_hipertensao', false);
+                  setValue('patologias_familia.fam_obesidade', false);
+                  setValue('patologias_familia.fam_dislipidemias', false);
+                  setValue('patologias_familia.fam_diabetes', false);
+                  setValue(
+                    'patologias_familia.fam_doenca_arterial_coronariana',
+                    false
+                  );
+                  setValue('patologias_familia.patologias_familia_outras', '');
+                }}
+              />
+            )}
+            name="patologias_familia.nao_tenho_patologias"
+            control={control}
+            defaultValue={false}
           />
-          <S.Input
-            disabled={doenca}
-            label="Outras Doenças"
-            mode="outlined"
-            ref={register('patologias_familia.patologias_familia_outras')}
-            onChangeText={(text) =>
-              setValue('patologias_familia.patologias_familia_outras', text)
-            }
+
+          <Controller
+            render={({ onChange, value }) => (
+              <S.Input
+                disabled={watch('patologias_familia.nao_tenho_patologias')}
+                label="Outro"
+                mode="outlined"
+                value={value}
+                onChangeText={(text) => onChange(text)}
+              />
+            )}
+            name="patologias_familia.patologias_familia_outras"
+            control={control}
+            defaultValue=""
           />
 
           <S.TitleInput>
             Faz uso contínuo de algum medicamento? Se sim qual?
           </S.TitleInput>
-          <S.Input
-            label="Medicamentos"
-            mode="outlined"
-            ref={register('medicamento_continuo')}
-            onChangeText={(text) => setValue('medicamento_continuo', text)}
+
+          <Controller
+            render={({ onChange, value }) => (
+              <S.Input
+                label="Medicamentos"
+                mode="outlined"
+                value={value}
+                onChangeText={(text) => onChange(text)}
+              />
+            )}
+            name="medicamento_continuo"
+            control={control}
+            defaultValue=""
           />
         </S.ContainerCheckbox>
 
