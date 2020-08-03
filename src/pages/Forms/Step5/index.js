93a1cb2f21a86e6de853da01308a2a2e3af8fff5
Otto F. Quintanilha <ottofq@gmail.com>
2020-08-03 18:40:02 -0300

    refactor step 5 form

--- src/pages/Forms/Step5/index.js
+++ src/pages/Forms/Step5/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { Button } from 'react-native-paper';
 import { useRoute, useNavigation } from '@react-navigation/native';
 
@@ -9,28 +9,10 @@ import ProgressBar from '../../../components/ProgressBar';
 import * as S from './styles';
 
 const Step5 = () => {
-  const { register, handleSubmit, setValue } = useForm();
-  const [doencaCardiovascular, setDoencaCardiovascular] = useState(false);
-  const [hipertensaoArterial, setHipertensaoArterial] = useState(false);
-  const [obesidade, setObesidade] = useState(false);
-  const [dislipidemias, setDislipidemias] = useState(false);
-  const [diabetes, setDiabetes] = useState(false);
-  const [doencaArterialCoronariana, setDoencaArterialCoronariana] = useState(
-    false
-  );
-  const [doenca, setDoenca] = useState(false);
+  const { handleSubmit, setValue, watch, control } = useForm();
+
   const navigation = useNavigation();
   const { params } = useRoute();
-
-  useEffect(() => {
-    setValue('patologias.doenca_cardiovascular', true);
-    setValue('patologias.hipertensao_arterial', false);
-    setValue('patologias.obesidade', false);
-    setValue('patologias.dislipidemias', false);
-    setValue('patologias.diabetes', false);
-    setValue('patologias.doenca_arterial_coronariana', false);
-    setValue('patologias.outras_patologias', false);
-  }, [setValue]);
 
   function handleButtonNext(data) {
     const obj = { ...params, ...data };
@@ -38,23 +20,14 @@ const Step5 = () => {
       ...obj,
     });
   }
-  function handlerCheckbox(field, state, setState) {
-    if (!doenca) {
-      setState(!state);
-      setValue(field, !state);
-    }
-  }
 
-  function handlerCheckboxDoenca() {
-    setDoenca(!doenca);
-    setValue('patologias.doenca_cardiovascular', true);
-    setValue('patologias.hipertensao_arterial', false);
-    setValue('patologias.obesidade', false);
-    setValue('patologias.dislipidemias', false);
-    setValue('patologias.diabetes', false);
-    setValue('patologias.doenca_arterial_coronariana', false);
-    setValue('patologias.outras_patologias', false);
-  }
+  const handleCheckboxStatus = (value) => {
+    if (watch('patologias.nao_tenho_patologias')) return 'indeterminate';
+
+    if (value) return 'checked';
+
+    return 'unchecked';
+  };
 
   function handleButtonPrev() {
     navigation.goBack();
@@ -68,137 +41,125 @@ const Step5 = () => {
           Você apresenta ou já apresentou alguma das patologias abaixo? Se sim,
           quais?
         </S.TitleCheckboxGroup>
-
-        <CheckBoxItem
-          label="Doença cardiovascular"
-          status={
-            doenca
-              ? 'indeterminate'
-              : 'unchecked' && doencaCardiovascular
-              ? 'checked'
-              : 'unchecked'
-          }
-          ref={register('patologias.doenca_cardiovascular')}
-          onPress={() =>
-            handlerCheckbox(
-              'patologias.doenca_cardiovascular',
-              doencaCardiovascular,
-              setDoencaCardiovascular
-            )
-          }
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Doença cardiovascular"
+              status={handleCheckboxStatus(value)}
+              onPress={() =>
+                setValue('patologias.doenca_cardiovascular', !value)
+              }
+            />
+          )}
+          name="patologias.doenca_cardiovascular"
+          control={control}
+          defaultValue={false}
         />
 
-        <CheckBoxItem
-          label="Hipertensão arterial"
-          status={
-            doenca
-              ? 'indeterminate'
-              : 'unchecked' && hipertensaoArterial
-              ? 'checked'
-              : 'unchecked'
-          }
-          ref={register('patologias.hipertensao_arterial')}
-          onPress={() =>
-            handlerCheckbox(
-              'patologias.hipertensao_arterial',
-              hipertensaoArterial,
-              setHipertensaoArterial
-            )
-          }
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Hipertensão arterial"
+              status={handleCheckboxStatus(value)}
+              onPress={() => setValue('patologias.hipertensao', !value)}
+            />
+          )}
+          name="patologias.hipertensao"
+          control={control}
+          defaultValue={false}
         />
 
-        <CheckBoxItem
-          label="Obesidade"
-          status={
-            doenca
-              ? 'indeterminate'
-              : 'unchecked' && obesidade
-              ? 'checked'
-              : 'unchecked'
-          }
-          ref={register('patologias.obesidade')}
-          onPress={() =>
-            handlerCheckbox('patologias.obesidade', obesidade, setObesidade)
-          }
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Obesidade"
+              status={handleCheckboxStatus(value)}
+              onPress={() => setValue('patologias.obesidade', !value)}
+            />
+          )}
+          name="patologias.obesidade"
+          control={control}
+          defaultValue={false}
         />
 
-        <CheckBoxItem
-          label="Dislipidemias"
-          status={
-            doenca
-              ? 'indeterminate'
-              : 'unchecked' && dislipidemias
-              ? 'checked'
-              : 'unchecked'
-          }
-          ref={register('patologias.dislipidemias')}
-          onPress={() =>
-            handlerCheckbox(
-              'patologias.dislipidemias',
-              dislipidemias,
-              setDislipidemias
-            )
-          }
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Dislipidemias"
+              status={handleCheckboxStatus(value)}
+              onPress={() => setValue('patologias.dislipidemias', !value)}
+            />
+          )}
+          name="patologias.dislipidemias"
+          control={control}
+          defaultValue={false}
         />
 
-        <CheckBoxItem
-          label="Diabetes"
-          status={
-            doenca
-              ? 'indeterminate'
-              : 'unchecked' && diabetes
-              ? 'checked'
-              : 'unchecked'
-          }
-          ref={register('patologias.diabetes')}
-          onPress={() =>
-            handlerCheckbox('patologias.diabetes', diabetes, setDiabetes)
-          }
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Diabetes"
+              status={handleCheckboxStatus(value)}
+              onPress={() => setValue('patologias.diabetes', !value)}
+            />
+          )}
+          name="patologias.diabetes"
+          control={control}
+          defaultValue={false}
         />
 
-        <CheckBoxItem
-          label="Doença Arterial Coronariana"
-          status={
-            doenca
-              ? 'indeterminate'
-              : 'unchecked' && doencaArterialCoronariana
-              ? 'checked'
-              : 'unchecked'
-          }
-          ref={register('patologias.doenca_arterial_coronariana')}
-          onPress={() =>
-            handlerCheckbox(
-              'patologias.doenca_arterial_coronariana',
-              doencaArterialCoronariana,
-              setDoencaArterialCoronariana
-            )
-          }
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Doença Arterial Coronariana"
+              status={handleCheckboxStatus(value)}
+              onPress={() =>
+                setValue('patologias.doenca_arterial_coronariana', !value)
+              }
+            />
+          )}
+          name="patologias.doenca_arterial_coronariana"
+          control={control}
+          defaultValue={false}
         />
 
-        <CheckBoxItem
-          label="Não possuo doenças"
-          status={
-            doencaCardiovascular ||
-            hipertensaoArterial ||
-            obesidade ||
-            dislipidemias ||
-            doencaArterialCoronariana ||
-            diabetes
-              ? 'indeterminate'
-              : 'unchecked' && doenca
-              ? 'checked'
-              : 'unchecked'
-          }
-          onPress={() => handlerCheckboxDoenca()}
+        <Controller
+          render={({ value }) => (
+            <CheckBoxItem
+              label="Não possuo patologias"
+              status={value ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setValue('patologias.nao_tenho_patologias', !value);
+                setValue('patologias.doenca_cardiovascular', false);
+                setValue('patologias.hipertensao', false);
+                setValue('patologias.obesidade', false);
+                setValue('patologias.dislipidemias', false);
+                setValue('patologias.diabetes', false);
+                setValue('patologias.doenca_arterial_coronariana', false);
+                setValue('patologias.outras_patologias', '');
+              }}
+            />
+          )}
+          name="patologias.nao_tenho_patologias"
+          control={control}
+          defaultValue={false}
+        />
+
+        <Controller
+          render={({ onChange, value }) => (
+            <S.Input
+              disabled={watch('patologias.nao_tenho_patologias')}
+              label="Outro"
+              mode="outlined"
+              value={value}
+              onChangeText={(text) => onChange(text)}
+            />
+          )}
+          name="patologias.outras_patologias"
+          control={control}
+          defaultValue=""
         />
       </S.ContainerCheckbox>
-      <S.Input
-        disabled={doenca}
-        label="Outras Doenças"
-        mode="outlined"
-        ref={register('patologias.outras_patologias')}
-        onChangeText={(text) => setValue('patologias.outras_patologias', text)}
-      />
 
       <S.ContainerButton>
         <Button mode="contained" onPress={handleButtonPrev}>
