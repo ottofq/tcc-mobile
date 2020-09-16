576b4480c293a624659991e943b8e2ec6ff85e88
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-15 23:43:36 -0300

    change reducer function to file

--- src/contexts/User/index.js
+++ src/contexts/User/index.js
@@ -1,103 +1,25 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-underscore-dangle */
-import React, { createContext, useReducer, useEffect } from 'react';
+import React, { createContext, useReducer } from 'react';
 import PropTypes from 'prop-types';
-import produce from 'immer';
+
 import AsyncStorage from '@react-native-community/async-storage';
 
 import USER_INITIAL_STATE from './initialStateStudent';
+import reducer from './reducer';
 
 const UserContext = createContext({
   user: USER_INITIAL_STATE,
   dispatch: () => {},
-  postStudent: () => {},
 });
 
-function userReducer(state, action) {
-  switch (action.type) {
-    case 'STUDENT:ADD_PROPS': {
-      return { ...state, ...action.payload };
-    }
-    case 'STUDENT:RU_RATING_PAGE_1': {
-      return produce(state, (draftState) => {
-        draftState.avaliacao_RU.aroma = action.payload.avaliacao_RU.aroma;
-        draftState.avaliacao_RU.coloracao_cardapio =
-          action.payload.avaliacao_RU.coloracao_cardapio;
-      });
-    }
-    case 'STUDENT:RU_RATING_PAGE_2': {
-      return produce(state, (draftState) => {
-        draftState.avaliacao_RU.textura_preparacao =
-          action.payload.avaliacao_RU.textura_preparacao;
-        draftState.avaliacao_RU.sabor_preparacao =
-          action.payload.avaliacao_RU.sabor_preparacao;
-      });
-    }
-
-    case 'STUDENT:LAST_STEP_FORM': {
-      return produce(state, (draftState) => {
-        draftState.avaliacao_RU.avaliacao_geral =
-          action.payload.avaliacao_RU.avaliacao_geral;
-        draftState.melhorias_RU.cardapio = action.payload.melhorias_RU.cardapio;
-        draftState.melhorias_RU.melhoria_sabor_preparacao =
-          action.payload.melhorias_RU.melhoria_sabor_preparacao;
-        draftState.melhorias_RU.opcao_vegetariana =
-          action.payload.melhorias_RU.opcao_vegetariana;
-        draftState.melhorias_RU.estrutura_fisica =
-          action.payload.melhorias_RU.estrutura_fisica;
-        draftState.melhorias_RU.tempo_fila =
-          action.payload.melhorias_RU.tempo_fila;
-        draftState.melhorias_RU.preco_ticket =
-          action.payload.melhorias_RU.preco_ticket;
-        draftState.melhorias_RU.melhoria_outros =
-          action.payload.melhorias_RU.melhoria_outros;
-      });
-    }
-
-    case 'STUDENT:CLEAN_PROPS': {
-      return produce(state, (draftState) => {
-        Object.keys(draftState).forEach((item) => {
-          if (draftState[item] === undefined) {
-            delete draftState[item];
-          }
-        });
-      });
-    }
-    case 'STUDENT:ADD_ID': {
-      return produce(state, (draftState) => {
-        draftState.id = action.payload.id;
-      });
-    }
-    case 'STUDENT:LOAD_FROM_STORAGE': {
-      return { ...state, ...action.payload };
-    }
-    case 'STUDENT:LOGOUT': {
-      return USER_INITIAL_STATE;
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`);
-    }
-  }
-}
-
 export const UserProvider = ({ children }) => {
-  const [user, dispatch] = useReducer(userReducer, USER_INITIAL_STATE);
-
-  useEffect(() => {
-    async function loadUserFromStorage() {
-      const storageUser = await AsyncStorage.getItem('@APP_RU:user');
-
-      if (storageUser) {
-        const parseUser = JSON.parse(storageUser);
-        dispatch({ type: 'STUDENT:LOAD_FROM_STORAGE', payload: parseUser });
-      }
-    }
-    loadUserFromStorage();
-  }, []);
+  const [user, dispatch] = useReducer(reducer, USER_INITIAL_STATE);
 
   async function persistUser(student) {
     const userToString = JSON.stringify(student);
     await AsyncStorage.setItem('@APP_RU:user', userToString);
+    dispatch({ type: 'STUDENT:LOAD_FROM_STORAGE', payload: student });
   }
 
   return (
