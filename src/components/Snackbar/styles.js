a1f2125242b0141f3a1aa82d8428395af984bcfa
Otto F. Quintanilha <ottofq@gmail.com>
2020-09-16 23:17:36 -0300

    add snackbar component

---
+++ src/components/Snackbar/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import { Snackbar as SnackbarPaper } from 'react-native-paper';
+
+export const Snackbar = styled(SnackbarPaper)`
+  background-color: #f33;
+`;
